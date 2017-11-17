14b0470a79637327d6387ea13e41ef2de5b3852f
Caelan Garrett <caelan@mit.edu>
2017-11-17 13:21:51 -0500

    Before I change MoveHandStraight

--- motion_planners/star_roadmap.py
+++ motion_planners/star_roadmap.py
@@ -1,9 +1,23 @@
-class StarRoadmap(object):
+from collections import Mapping
+
+#class StarRoadmap(Mapping, object):
+class StarRoadmap(Mapping, object):
 
     def __init__(self, center, planner):
         self.center = center
         self.planner = planner
         self.roadmap = {}
+
+    """
+    def __getitem__(self, q):
+        return self.roadmap[q]
+
+    def __len__(self):
+        return len(self.roadmap)
+
+    def __iter__(self):
+        return iter(self.roadmap)
+    """
 
     def grow(self, goal):
         if goal not in self.roadmap:
@@ -17,4 +31,4 @@ class StarRoadmap(object):
         goal_traj = self.grow(goal)
         if goal_traj is None:
             return None
-        return (start_traj.reverse(), goal_traj)
+        return start_traj.reverse(), goal_traj
