81f1a34734cf83de0acaa57c40cbe76eca364f00
Caelan Garrett <caelan@mit.edu>
2019-02-27 11:20:57 -0500

    Asymmetric extend fn

--- motion_planners/rrt_connect.py
+++ motion_planners/rrt_connect.py
@@ -2,45 +2,52 @@ from .smoothing import smooth_path
 from .rrt import TreeNode, configs
 from .utils import irange, argmin, RRT_ITERATIONS, RRT_RESTARTS, RRT_SMOOTHING
 
+def asymmetric_extend(q1, q2, extend_fn, backward=False):
+    if backward:
+        return reversed(list(extend_fn(q2, q1)))
+    return extend_fn(q1, q2)
 
-def rrt_connect(q1, q2, distance, sample, extend, collision, iterations=RRT_ITERATIONS):
-    if collision(q1) or collision(q2):
+def rrt_connect(q1, q2, distance_fn, sample_fn, extend_fn, collision_fn, iterations=RRT_ITERATIONS):
+    # TODO: collision(q1, q2)
+    if collision_fn(q1) or collision_fn(q2):
         return None
     root1, root2 = TreeNode(q1), TreeNode(q2)
     nodes1, nodes2 = [root1], [root2]
-    for _ in irange(iterations):
-        if len(nodes1) > len(nodes2):
+    for iteration in irange(iterations):
+        swap = len(nodes1) > len(nodes2)
+        if swap:
             nodes1, nodes2 = nodes2, nodes1
-        s = sample()
+        s = sample_fn()
 
-        last1 = argmin(lambda n: distance(n.config, s), nodes1)
-        for q in extend(last1.config, s):
-            if collision(q):
+        last1 = argmin(lambda n: distance_fn(n.config, s), nodes1)
+        for q in asymmetric_extend(last1.config, s, extend_fn, swap):
+            if collision_fn(q):
                 break
             last1 = TreeNode(q, parent=last1)
             nodes1.append(last1)
 
-        last2 = argmin(lambda n: distance(n.config, last1.config), nodes2)
-        for q in extend(last2.config, last1.config):
-            if collision(q):
+        last2 = argmin(lambda n: distance_fn(n.config, last1.config), nodes2)
+        for q in asymmetric_extend(last2.config, last1.config, extend_fn, not swap):
+            if collision_fn(q):
                 break
             last2 = TreeNode(q, parent=last2)
             nodes2.append(last2)
         else:
             path1, path2 = last1.retrace(), last2.retrace()
-            if path1[0] != root1:
+            if swap:
                 path1, path2 = path2, path1
+            #print('{} iterations, {} nodes'.format(iteration, len(nodes1) + len(nodes2)))
             return configs(path1[:-1] + path2[::-1])
     return None
 
 # TODO: version which checks whether the segment is valid
 
-def direct_path(q1, q2, extend, collision):
-    if collision(q1) or collision(q2):
+def direct_path(q1, q2, extend_fn, collision_fn):
+    if collision_fn(q1) or collision_fn(q2):
         return None
     path = [q1]
-    for q in extend(q1, q2):
-        if collision(q):
+    for q in extend_fn(q1, q2):
+        if collision_fn(q):
             return None
         path.append(q)
     return path
@@ -53,10 +60,11 @@ def birrt(q1, q2, distance, sample, extend, collision,
     path = direct_path(q1, q2, extend, collision)
     if path is not None:
         return path
-    for _ in irange(restarts + 1):
+    for attempt in irange(restarts + 1):
         path = rrt_connect(q1, q2, distance, sample, extend,
                            collision, iterations=iterations)
         if path is not None:
+            #print('{} attempts'.format(attempt))
             if smooth is None:
                 return path
             return smooth_path(path, extend, collision, iterations=smooth)
