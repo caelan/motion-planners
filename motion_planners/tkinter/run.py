27782058254b6d293e71d363d0dfdf4581484b92
caelan <caelan@mit.edu>
2021-03-13 00:08:39 -0500

    Cost analysis

--- motion_planners/tkinter/run.py
+++ motion_planners/tkinter/run.py
@@ -2,25 +2,33 @@ from __future__ import print_function
 
 import numpy as np
 import math
+import time
+
+from itertools import islice
 
 from motion_planners.tkinter.viewer import sample_box, get_distance, is_collision_free, \
     create_box, draw_solution, draw_roadmap, draw_environment, point_collides, sample_line, add_points, \
     add_segments, add_roadmap, get_box_center
-from motion_planners.utils import user_input, pairs, profiler
-from motion_planners.rrt_connect import birrt
+from motion_planners.utils import user_input, pairs, profiler, irange, elapsed_time, INF
+from motion_planners.rrt_connect import birrt, direct_path, rrt_connect, smooth_path
 
 
 ##################################################
 
 def get_sample_fn(region, obstacles=[]):
     samples = []
+    collision_fn = get_collision_fn(obstacles)
 
     def region_gen():
         #lower, upper = region
         #area = np.product(upper - lower) # TODO: sample proportional to area
-        q = sample_box(region)
-        samples.append(q)
-        return q # TODO: sampling with state (e.g. deterministic sampling)
+        while True:
+            q = sample_box(region)
+            if collision_fn(q):
+                continue
+            samples.append(q)
+            return q # TODO: sampling with state (e.g. deterministic sampling)
+
     return region_gen, samples
 
 def get_connected_test(obstacles, max_distance=0.25): # 0.25 | 0.2 | 0.25 | 0.5 | 1.0
@@ -48,12 +56,15 @@ def get_threshold_fn():
 def get_collision_fn(obstacles):
     return lambda q: point_collides(q, obstacles)
 
-def get_extend_fn():
+def get_extend_fn(obstacles=[]):
     roadmap = []
+    collision_fn = get_collision_fn(obstacles)
 
     def extend_fn(q1, q2):
         path = [q1]
         for q in sample_line(segment=(q1, q2)):
+            #if collision_fn(q):
+            #    return
             yield q
             roadmap.append((path[-1], q))
             path.append(q)
@@ -73,7 +84,9 @@ def main(max_time=20):
     np.set_printoptions(precision=3)
 
     obstacles = [
-        create_box(center=(.5, .5), extents=(.25, .25)),
+        create_box(center=(.35, .75), extents=(.25, .25)),
+        create_box(center=(.75, .35), extents=(.225, .225)),
+        create_box(center=(.5, .5), extents=(.225, .225)),
     ]
 
     # TODO: alternate sampling from a mix of regions
@@ -82,7 +95,7 @@ def main(max_time=20):
         'green': create_box(center=(.8, .8), extents=(.1, .1)),
     }
 
-    q0 = np.array([0., 0.])
+    start = np.array([0., 0.])
     goal = 'green'
     if isinstance(goal, str) and (goal in regions):
         goal = get_box_center(regions[goal])
@@ -97,19 +110,20 @@ def main(max_time=20):
     #connected_test, roadmap = get_connected_test(obstacles)
     collision_fn = get_collision_fn(obstacles)
     distance_fn = get_distance
-    extend_fn, roadmap = get_extend_fn()
+    extend_fn, roadmap = get_extend_fn(obstacles=obstacles) # obstacles | []
 
-    with profiler():
-        path = birrt(q0, goal, distance=distance_fn, sample=sample_fn,
-                     extend=extend_fn, collision=collision_fn, smooth=1000) #, **kwargs)
+    with profiler():      
+        path = rrt_connect(start, goal, distance_fn, sample_fn, extend_fn, collision_fn,
+                           iterations=1000, tree_frequency=1, max_time=INF) #, **kwargs)
+        #path = birrt(start, goal, distance=distance_fn, sample=sample_fn,
+        #             extend=extend_fn, collision=collision_fn, smooth=1000) #, **kwargs)
 
-    #samples = list(islice(region_gen('env'), 100))
-    #prm = DegreePRM(distance=get_distance, extend=None, collision=get_collision_fn(obstacles),
-    #                samples=samples, connect_distance=.25)
+        #samples = list(islice(region_gen('env'), 100))
+        #prm = DegreePRM(distance=get_distance, extend=None, collision=get_collision_fn(obstacles),
+        #                samples=samples, connect_distance=.25)
 
     #########################
 
-    print(path)
     add_roadmap(viewer, roadmap, color='black')
     add_points(viewer, samples, color='blue')
 
@@ -118,7 +132,15 @@ def main(max_time=20):
         return
 
     segments = list(pairs(path))
+    print('Distance: {:.3f}'.format(sum(distance_fn(*pair) for pair in segments)))
     add_segments(viewer, segments, color='green')
+
+    extend_fn, roadmap = get_extend_fn(obstacles=obstacles) # obstacles | []
+    path = smooth_path(path, extend_fn, collision_fn, iterations=100)
+    segments = list(pairs(path))
+    print('Distance: {:.3f}'.format(sum(distance_fn(*pair) for pair in segments)))
+
+    add_segments(viewer, segments, color='red')
     user_input('Finish?')
 
 
