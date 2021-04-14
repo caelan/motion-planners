a6af5fcc820b50e3e13092113a861d34d186efe0
caelan <caelan@mit.edu>
2021-04-14 17:21:15 -0400

    Randomize shortcut collisions

--- motion_planners/smoothing.py
+++ motion_planners/smoothing.py
@@ -1,15 +1,16 @@
 from random import randint, random
 from motion_planners.utils import INF, elapsed_time, irange, waypoints_from_path, pairs, get_distance, \
-    convex_combination, flatten
+    convex_combination, flatten, randomize
 
 import time
 import numpy as np
 
-def smooth_path_old(path, extend, collision, iterations=50, max_tine=INF):
+def smooth_path_old(path, extend, collision, iterations=50, max_time=INF):
+    assert (iterations < INF) or (max_time < INF)
     start_time = time.time()
     smoothed_path = path
     for _ in irange(iterations):
-        if elapsed_time(start_time) > max_tine:
+        if elapsed_time(start_time) > max_time:
             break
         if len(smoothed_path) <= 2:
             return smoothed_path
@@ -20,20 +21,21 @@ def smooth_path_old(path, extend, collision, iterations=50, max_tine=INF):
         if j < i:
             i, j = j, i
         shortcut = list(extend(smoothed_path[i], smoothed_path[j]))
-        if (len(shortcut) < (j - i)) and all(not collision(q) for q in shortcut):
+        if (len(shortcut) < (j - i)) and all(not collision(q) for q in randomize(shortcut)):
             smoothed_path = smoothed_path[:i + 1] + shortcut + smoothed_path[j + 1:]
     return smoothed_path
 
-def smooth_path(path, extend, collision, distance_fn=None, iterations=50, max_tine=INF):
+def smooth_path(path, extend, collision, distance_fn=None, iterations=50, max_time=INF):
     # TODO: makes an assumption on the distance metric
     # TODO: smooth until convergence
+    assert (iterations < INF) or (max_time < INF)
     start_time = time.time()
     if distance_fn is None:
         distance_fn = get_distance
     waypoints = path
     for _ in irange(iterations):
         waypoints = waypoints_from_path(waypoints)
-        if (elapsed_time(start_time) > max_tine) or (len(waypoints) <= 2):
+        if (elapsed_time(start_time) > max_time) or (len(waypoints) <= 2):
             break
 
         indices = list(range(len(waypoints)))
@@ -51,7 +53,7 @@ def smooth_path(path, extend, collision, distance_fn=None, iterations=50, max_ti
             segment1, segment2 = segment2, segment1
         point1, point2 = [convex_combination(waypoints[i], waypoints[j], w=random())
                           for i, j in [segment1, segment2]]
-        if all(not collision(q) for q in extend(point1, point2)):
+        if all(not collision(q) for q in randomize(extend(point1, point2))):
             i, _ = segment1
             _, j = segment2
             waypoints = waypoints[:i + 1] + [point1, point2] + waypoints[j:]
